9473799d068ea1dda785ec95d6f7b99e4f6331b7
emranweb <emranbd77@gmail.com>
2023-05-26 10:43:35 +0600

    add typescript to this started

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+    return (
+        <div className="App">
+            <div className="container mx-auto">
+                <h1 className="text-6xl text-center font-poppins font-semibold leading-normal mt-4">
+                    TailwindCSS + ReactJs + Vite Starter Template
+                </h1>
+            </div>
+        </div>
+    );
+}
+
+export default App;
